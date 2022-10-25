5d0c97361479a79761a33354b9dfc6603c73a817
shahira-sadat <shahira.sadat1@gmail.com>
2022-10-25 17:49:27 +0430

    Add routes and views

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,9 @@ module Blog
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators do |g|
+      g.test_framework = :rspec
+    end
   end
 end

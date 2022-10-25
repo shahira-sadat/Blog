5cbdfd4ed6d5075c7264ee6b3020684e8f5eec3b
shahira-sadat <shahira.sadat1@gmail.com>
2022-10-25 17:09:34 +0430

    Create view and controller of posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  get 'authors/index'
   get 'authors/show'
+  get 'authors/index'
+  root 'authors#index'
+  resources :authors, only: [:index, :show] do
+    resources :posts, only: [:index, :show] do
+    end
+  end
+  # get 'posts/index'
+  # get 'posts/show'
+  # get 'authors/index'
+  # get 'authors/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

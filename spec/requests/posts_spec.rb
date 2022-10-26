a51dd5579e48f7c2288611077740b4d8bbc24f96
shahira-sadat <shahira.sadat1@gmail.com>
2022-10-26 21:14:06 +0430

    Add test for authors

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,16 +2,33 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
+    before(:each) do
+      get '/authors/:author_id/posts/'
+    end
+
     it 'returns http success' do
-      get '/posts/index'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'renders correct template text' do
+      expect(response.body).to include('User posts')
+    end
+    it 'Response status is 200' do
+      get root_path
+      expect(response).to have_http_status(200)
     end
   end
 
   describe 'GET /show' do
+    before(:each) do
+      get '/authors/:author_id/posts/:id'
+    end
     it 'returns http success' do
-      get '/posts/show'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'renders correct template text' do
+      expect(response.body).to include('Here is a list of posts for a given user')
     end
   end
 end

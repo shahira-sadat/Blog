7c12aa1596de352cd02c50a6486bf25e4a6b0bde
shahira-sadat <shahira.sadat1@gmail.com>
2022-11-01 22:04:40 +0430

    Update

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,29 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe 'Posts', type: :request do
-#   describe 'GET /index' do
-#     pending "add some examples (or delete) #{__FILE__}"
-#   end
-# end
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /index' do
+    before(:all) do
+      @author1 = Author.create!(name: 'Navid',
+                                photo: 'https://mobile.twitter.com/sm_sayedi/photo',
+                                bio: 'Teacher from Kabul.')
+      post1 = Post.create!(author: @author1, title: 'Post 1', text: 'text Post 1')
+    end
+    before(:each) do
+      get author_posts_path(@author1)
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders correct template text' do
+      expect(response.body).to include('Post Counter')
+    end
+  end
+
+  describe 'GET /show' do
+    before(:each) do
+        get author_posts_path(@author1)
+    end
+  end
+end

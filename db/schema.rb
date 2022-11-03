e4c6624893b5f375e5aae7357a4aadfa5cc2ea6e
shahira-sadat <shahira.sadat1@gmail.com>
2022-11-03 18:18:29 +0430

    Update the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_02_165312) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_03_134540) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_02_165312) do
     t.datetime "confirmed_at"
     t.string "confirmation_token"
     t.datetime "confirmation_sent_at"
+    t.string "role", default: "read", null: false
     t.index ["email"], name: "index_authors_on_email", unique: true
     t.index ["reset_password_token"], name: "index_authors_on_reset_password_token", unique: true
   end
